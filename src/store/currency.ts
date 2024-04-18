f31be467494e005cca5ce2493e9d37c24d41d970
evgenius <mr.odnovil@mail.ru>
2024-04-18 20:09:58 +1000

    removed debug state watchers

--- src/store/currency.ts
+++ src/store/currency.ts
@@ -1,4 +1,4 @@
-import {createStore, createEvent} from 'effector';
+import { createEvent, restore } from 'effector';
 import {Currency} from "@/types/Currency.ts";
 import {useUnit} from "effector-react";
 
@@ -11,35 +11,16 @@ export const initialTo: Currency = {
     code: "RUB"
 }
 
-
 export const currencyFromSelected = createEvent<Currency>();
-
-
-export const $currencyFrom =
-    createStore<Currency>(initialFrom)
-        .on(currencyFromSelected, (_, selectedCurrency) => selectedCurrency)
-
-// $currencyFrom.watch((state) => {
-//     console.log(state)
-// })
+export const $currencyFrom = restore(currencyFromSelected, initialFrom)
 
 
 export const currencyToSelected = createEvent<Currency>();
-
-export const $currencyTo =
-    createStore<Currency>(initialTo)
-        .on(currencyToSelected, (_, selectedCurrency) => selectedCurrency)
-
-// $currencyTo.watch((state) => {
-//     console.log(state)
-// })
+export const $currencyTo = restore(currencyToSelected, initialTo)
 
 
 export const setIsSwitched = createEvent<boolean>()
-
-export const $isSwitched =
-    createStore<boolean>(false)
-        .on(setIsSwitched, (_, state) => state)
+export const $isSwitched = restore(setIsSwitched, false)
 
 
 export const useCurrencies = () => useUnit({
