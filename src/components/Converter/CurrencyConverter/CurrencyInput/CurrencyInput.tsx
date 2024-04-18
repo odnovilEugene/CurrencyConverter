e729d371c1b71f64943f74d388d6dd5f309295b0
evgenius <mr.odnovil@mail.ru>
2024-04-18 20:12:29 +1000

    removed validation algo

--- src/components/Converter/CurrencyConverter/CurrencyInput/CurrencyInput.tsx
+++ src/components/Converter/CurrencyConverter/CurrencyInput/CurrencyInput.tsx
@@ -1,5 +1,6 @@
 import {ChangeEvent, FC, useEffect, useState} from 'react';
 import s from "components/Converter/CurrencyConverter/CurrencyInput/CurrencyInput.module.scss"
+import { validateInput } from '@/utils/validateInput.ts';
 
 type Props = {
     value: number,
@@ -11,40 +12,18 @@ const CurrencyInput: FC<Props> = ({
     valueSelected,
 }) => {
 
-    const [localValue, setLocalValue] = useState<string>('0')
+    const [localValue, setLocalValue] = useState('0')
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        let inputValue = event.target.value
 
-        inputValue = inputValue.replace(/[^0-9.,]/g, '')
+        const validatedInput = validateInput(event.target.value)
 
-        inputValue = inputValue.replace(',', '.')
-
-        if (inputValue.indexOf('.') === 0) {
-            inputValue = '0' + inputValue
-        }
-
-        if (/^0[0-9]/.test(inputValue)) {
-            inputValue = inputValue.substr(1)
-        }
-
-        const dotIndex = inputValue.indexOf('.')
-        const commaIndex = inputValue.indexOf(',')
-
-        if (dotIndex !== -1 && commaIndex !== -1) {
-            inputValue = inputValue.replace(',', '')
-        }
-
-        if (inputValue.indexOf('.') !== inputValue.lastIndexOf('.')) {
-            inputValue = inputValue.slice(0, -1)
-        }
-
-        if (inputValue === '') {
+        if (validatedInput === '') {
             setLocalValue('')
             valueSelected(0)
         } else {
-            setLocalValue(inputValue)
-            valueSelected(parseFloat(inputValue))
+            setLocalValue(validatedInput)
+            valueSelected(parseFloat(validatedInput))
         }
     }
 
